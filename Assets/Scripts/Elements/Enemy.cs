1b17ea66d21db4f57c6de1f0ab07794a1d2b3731
gokcegun <gokce@locomotive.games>
2024-12-23 17:19:07 +0300

    Ders 20

--- Assets/Scripts/Elements/Enemy.cs
+++ Assets/Scripts/Elements/Enemy.cs
@@ -1,6 +1,7 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.AI;
@@ -12,8 +13,12 @@ public class Enemy : MonoBehaviour
     private Rigidbody _rb;
     public NavMeshAgent navMeshAgent;
     private Animator _animator;
+    public Transform zPrefab;
 
     private bool _isWalking;
+
+    private Transform _z1;
+    private Transform _z2;
 
     public void StartEnemy(Player player)
     {
@@ -21,6 +26,22 @@ public class Enemy : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
         _animator = GetComponentInChildren<Animator>();
         transform.Rotate(0,Random.Range(-180,180),0);
+        CreateAndAnimateZ();
+    }
+
+    private void CreateAndAnimateZ()
+    {
+        _z1 = Instantiate(zPrefab);
+        _z1.position = transform.position + Vector3.up * 2;
+        _z1.localScale = Vector3.zero;
+        _z1.DOMoveY(_z1.transform.position.y + 1, 1f).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
+        _z1.DOScale(1, 1f).SetLoops(-1, LoopType.Restart);
+
+        _z2 = Instantiate(zPrefab);
+        _z2.position = transform.position + Vector3.up * 2;
+        _z2.localScale = Vector3.zero;
+        _z2.DOMoveY(_z2.transform.position.y + 1, 1f).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart).SetDelay(.5f);
+        _z2.DOScale(1, 1f).SetLoops(-1, LoopType.Restart).SetDelay(.5f);
     }
 
     private void Update()
@@ -32,6 +53,10 @@ public class Enemy : MonoBehaviour
             {
                 _isWalking = true;
                 _animator.SetTrigger("Walk");
+                _z1.DOKill();
+                _z2.DOKill();
+                Destroy(_z1.gameObject);
+                Destroy(_z2.gameObject);
             }
         }
     }
